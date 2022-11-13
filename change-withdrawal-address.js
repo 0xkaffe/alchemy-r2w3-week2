6f607e826d55506aebfa6e711ea31d3d44d551ee
Dr. Brown in Crypto <117237823+0xkaffe@users.noreply.github.com>
2022-11-13 10:39:38 +0900

    Add files via upload

---
+++ change-withdrawal-address.js
@@ -0,0 +1,42 @@
+// scripts/change-withdrawal-address.js
+
+const hre = require("hardhat");
+const abi = require("../artifacts/contracts/BuyMeACoffee.sol/BuyMeACoffee.json");
+
+async function getBalance(provider, address) {
+  const balanceBigInt = await provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function main() {
+  // Get the contract that has been deployed to Goerli.
+  const contractAddress="0x6069d83FD9DFF978ceBf5E1829a153562586eC3D";
+  const contractABI = abi.abi;
+
+  // Get the node connection and wallet connection.
+  const provider = new hre.ethers.providers.AlchemyProvider("goerli", process.env.GOERLI_API_KEY);
+
+  // Ensure that signer is the SAME address as the original contract deployer,
+  // or else this script will fail with an error.
+  const signer = new hre.ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+  // Instantiate connected contract.
+  const buyMeACoffee = new hre.ethers.Contract(contractAddress, contractABI, signer);
+
+  // ==================================================================================
+  const newAddress = "0xA2da502C6794d95aEE721204b52AD9F0F634F111";
+  const changeAddress = await buyMeACoffee.changeWithdrawalAddress(newAddress);
+  await changeAddress.wait();
+
+  console.log("Your withdrawal address is changed to: ", await buyMeACoffee.returnWithdrawalAddress());
+  // ==================================================================================
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
